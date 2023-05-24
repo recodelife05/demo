feb3cd6610a1c7c4d74636212bd3ea965effb33d
Mark Anthony Laqui <mark.laqui@randemgroup.com>
2023-05-24 19:28:50 +0800

    added validation if user input is null or empty.

--- src/main/java/com/example/demo/infrastructure/ValidationHelper.java
+++ src/main/java/com/example/demo/infrastructure/ValidationHelper.java
@@ -2,6 +2,11 @@ package com.example.demo.infrastructure;
 
 public class ValidationHelper {
     public static Boolean Validate(String someText){
+
+        if(someText.isEmpty() || someText.equals(null) || someText.isBlank()){
+            return  false;
+        }
+
         try {
             int parseInt =  Integer.parseInt(someText);
             return false;
