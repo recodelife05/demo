00578378b722ffa24c947f2991144de98aa98893
Mark Anthony Laqui <mark.laqui@randemgroup.com>
2023-05-24 20:21:05 +0800

    refactor validation helper.

--- src/main/java/com/example/demo/infrastructure/ValidationHelper.java
+++ src/main/java/com/example/demo/infrastructure/ValidationHelper.java
@@ -1,11 +1,13 @@
 package com.example.demo.infrastructure;
 
+import java.util.Optional;
+
 public class ValidationHelper {
     public static Boolean Validate(String someText){
 
-        if(someText.isEmpty() || someText.equals(null) || someText.isBlank()){
-            return  false;
-        }
+        if(IsNullOrEmptyOrBlank(someText)){
+            return false;
+        };
 
         try {
             int parseInt =  Integer.parseInt(someText);
@@ -13,5 +15,11 @@ public class ValidationHelper {
         } catch (NumberFormatException ex) {
             return true;
         }
+    }
+    public static Boolean IsNullOrEmptyOrBlank(String someText){
+        if(someText.isEmpty() || someText.equals(null) || someText.isBlank()){
+            return  true;
+        }
+        return false;
     }
 }
